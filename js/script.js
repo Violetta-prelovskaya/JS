d48deba3ebb1ea0aa6c2af0a2293d473ab4f6e68
Violetta Prelovskaya <viprelova@gmail.com>
2020-11-01 14:02:25 +0300

    Task-2 (3)
    Присовение переменной вынесено на первую строчку.
    С помощью переменной page через метод classList сперва удален, а затем добавлен класс 'second-page'

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
-document.querySelector('.page').classList.remove('second-page');
 let page = document.querySelector('.page');
+page.classList.remove('second-page');
+page.classList.add('second-page');
