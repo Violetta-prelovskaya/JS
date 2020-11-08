608f7d38a93baff6efd1a89127a9b711d498bcb1
Violetta Prelovskaya <viprelova@gmail.com>
2020-11-09 00:11:37 +0300

    Элемент p помещен в переменную paragraph. Для дальнейшей работы элементу form также присвоена переменная

--- js/script.js
+++ js/script.js
@@ -3,3 +3,8 @@
 // Элемент input помещен в одноименную переменную. Значение input выведено в консоль
 let input = document.querySelector('input');
 console.log(input.value);
+
+// Элемент p помещен в переменную paragraph.
+// Для дальнейшей работы элементу form также присвоена переменная
+let paragraph = document.querySelector('p');
+let form = document.querySelector('form');
