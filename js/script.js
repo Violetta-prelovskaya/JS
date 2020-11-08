ae5b83ce343c7443a745fbb3b8f98b1913ec23ad
Violetta Prelovskaya <viprelova@gmail.com>
2020-11-09 00:08:47 +0300

    Элемент input помещен в одноименную переменную. Значение input выведено в консоль

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 //document.querySelector('.page').classList.remove('second-page');
 
+// Элемент input помещен в одноименную переменную. Значение input выведено в консоль
+let input = document.querySelector('input');
+console.log(input.value);
