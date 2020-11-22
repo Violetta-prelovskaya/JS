195464ee92cb02a4674dd233c5fbb89a819db017
Violetta Prelovskaya <viprelova@gmail.com>
2020-11-22 22:38:39 +0300

    Создание переменной и константы. Написание функции showMessage с последующим ее вызовом

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
-document.querySelector('.page').classList.remove('second-page');
+// document.querySelector('.page').classList.remove('second-page');
 
+// Задаем переменную name и константу birthYear
+let name = 'Violetta';
+const birthYear = 1994;
+// Прописываем функцию showMessage для выведения приветственного послания пользователю
+function showMessage(name) {
+    let message = 'Приветствую тебя, ' + name;
+
+    alert(message);
+};
+// Вызываем функцию
+showMessage(name);
