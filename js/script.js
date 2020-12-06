da255b05f7601c0087ddb1f47d716889b229cf5d
Violetta Prelovskaya <viprelova@gmail.com>
2020-12-06 19:16:13 +0300

    Реализована функция range

--- js/script.js
+++ js/script.js
@@ -11,3 +11,17 @@ function showMessage(name) {
 };
 // Вызываем функцию
 showMessage(name);
+
+/* Создаем функцию range с параметрами start и end. 
+* В теле функции создаем пустой массив result, в который с каждой итерацией цикла будет записываться число i, начиная от start и заканчивая end.
+*/
+function range(start, end) {
+    const result = [];
+    for(let i = start; i <= end; i++) {
+        result.push(i);
+    }
+    return result;
+}
+
+// Выводим функцию range через alert
+alert(range(15, 30));
